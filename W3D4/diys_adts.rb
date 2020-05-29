46a96582e150ffee21a0f489dfd01e14a1cfbb4e
SVKratzer <samuel.kratzer1894@gmail.com>
2020-05-28 22:38:22 -0400

    Add a whole heck of a lot of stuff, forgot I forgot the old commit every five minutes rule, but I'll get 'em next time

--- W3D4/diys_adts.rb
+++ W3D4/diys_adts.rb
@@ -24,3 +24,64 @@ class Stack
 end
 
 # EXERCISE 2
+
+class Queue
+
+  def initialize
+    @queue = []
+  end
+
+  def enqueue(el)
+    @queue.unshift(el)
+  end
+
+  def dequeue
+    @queue.shift
+  end
+
+  def peek
+    @queue.first
+  end
+
+  def inspect
+    @queue
+  end
+
+end
+
+# EXERCISE 3
+
+class Map
+
+  def initialize
+    @map = []
+  end
+
+  def set(key, value)
+    @map.each do |pair|
+      pair[1] = value if pair[0] == key
+      return
+    end
+    @map << [key, value]
+  end
+
+  def get(key)
+
+  end
+
+  def delete(key)
+
+  end
+
+  def show
+    @map
+  end
+
+end
+
+map = Map.new
+map.set("ferret", 1)
+map.set("dog", 2)
+map.set("cat", 3)
+map.set("parrot", 4)
+p map.show
