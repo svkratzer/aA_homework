ca21632acafd0eb44849cd6f22e347df78edad84
SVKratzer <samuel.kratzer1894@gmail.com>
2020-05-31 21:31:44 -0400

    Create GraphNode class with setters and getters

---
+++ W4D1/graph_nodes_and_bfs.rb
@@ -0,0 +1,19 @@
+class GraphNode
+  attr_accessor :neighbors
+  attr_reader :value
+  def initialize(value)
+    @value = value
+    @neighbors = []
+  end
+end
+
+a = GraphNode.new('a')
+b = GraphNode.new('b')
+c = GraphNode.new('c')
+d = GraphNode.new('d')
+e = GraphNode.new('e')
+f = GraphNode.new('f')
+a.neighbors = [b, c, e]
+c.neighbors = [b, d]
+e.neighbors = [a]
+f.neighbors = [e]
