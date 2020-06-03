43c3662e994f91c90056c9e8b7b2ade036e99118
SVKratzer <samuel.kratzer1894@gmail.com>
2020-06-02 22:12:52 -0400

    Complete #initialize and #add_random_color instance methods

--- W4D3/simon/lib/simon.rb
+++ W4D3/simon/lib/simon.rb
@@ -4,7 +4,9 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 
   def play
@@ -16,7 +18,7 @@ class Simon
   end
 
   def show_sequence
-
+    
   end
 
   def require_sequence
@@ -24,7 +26,7 @@ class Simon
   end
 
   def add_random_color
-
+    self.seq << COLORS.sample
   end
 
   def round_success_message
