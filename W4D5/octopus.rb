7f08411357fabd2743ab0d3e502501d71eeaff0c
SVKratzer <samuel.kratzer1894@gmail.com>
2020-06-04 22:13:58 -0400

    Implement dominant octopus

--- W4D5/octopus.rb
+++ W4D5/octopus.rb
@@ -16,16 +16,27 @@ def sluggish_octopus(fishes)
 end
 
 def dominant_octopus(fishes)
+  arr_length = fishes.length 
+  return fishes if arr_length <= 1
+  mid = arr_length / 2
+  arr = fishes.dup
+  left = arr.take(mid)
+  right = arr.drop(mid)
 
+  sorted_left = dominant_octopus(left)
+  sorted_right = dominant_octopus(right)
+
+  merge(sorted_left, sorted_right)
 end
 
 def merge(arr1, arr2)
   merged = []
   until arr1.empty? || arr2.empty?
-    merged << ( arr1.first < arr2.first ? arr1.shift : arr2.shift )
+    merged << ( arr1.first.length < arr2.first.length ? arr1.shift : arr2.shift )
   end
-  merged
+  merged + arr1 + arr2
 end
 
 p "fiiiissshhhhhh"
 sluggish_octopus(fishes)
+p dominant_octopus(fishes).last
