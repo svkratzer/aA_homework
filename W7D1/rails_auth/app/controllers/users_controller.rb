27f9a801daa42cd106c72264193796f2c5dd95a0
SVKratzer <samuel.kratzer1894@gmail.com>
2020-06-21 21:14:20 -0400

    Add show and index views to views/users

--- W7D1/rails_auth/app/controllers/users_controller.rb
+++ W7D1/rails_auth/app/controllers/users_controller.rb
@@ -1,12 +1,26 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.all
+    render :index
+  end
+
+  def show
+    @user = User.findy_by(id: params[:id])
+    if @user
+      render :show
+    else
+      redirect_to users_url
+    end
+  end
+
   def create
     @user = User.new(user_params)
     
     if @user.save
-      render json: @user
+      redirect_to user_url(@user)
     else
-      render json: @user.errors.full_messages
+      render :new
     end
   end
 
