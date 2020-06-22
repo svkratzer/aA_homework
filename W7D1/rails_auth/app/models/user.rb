5db3221675827eee5af1288e1b995d5275668d53
SVKratzer <samuel.kratzer1894@gmail.com>
2020-06-21 20:57:15 -0400

    Create SessionsController

--- W7D1/rails_auth/app/models/user.rb
+++ W7D1/rails_auth/app/models/user.rb
@@ -1,10 +1,18 @@
 class User < ApplicationRecord
+
+  validates :username, presence: true, uniqueness: true
+  validates :password_digest, presence: { message: 'Password can\'t be blank.'}
+  valditaes :password, length: { minimum: 6, allow_nil: true}
   
   def password=(password)
+    @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
 
   def is_password?(password)
     BCrypt::Password.new(self.password_digest).is_password?([password)
   end
+
+  private
+  attr_reader :password
 end
